cb5cd7d22532e7d78418af40815257000c920993
diego-alkmim <diego.alkmim@pathbit.com.br>
2025-08-24 00:13:21 -0300

    feat: add Header component and banner

---
+++ app/_components/header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="flex flex-row justify-between items-center p-5">
+        <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+        <Button
+          aria-label="Abrir menu lateral"
+          data-testId="button-menu-home"
+          size="icon"
+          variant="outline"
+        >
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 const Home = () => {
-  return <Button>Click me</Button>;
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Diego!</h2>
+        <p>Domingo, 23 de Agosto.</p>
+
+        <div className="flex items-center gap-2 mt-6">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative w-full h-[150px] mt-6">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
