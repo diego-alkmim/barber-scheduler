daa226068ece1593fe7c8405bffb29961f2cb982
diego-alkmim <diego.alkmim@pathbit.com.br>
2025-08-24 00:58:32 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,20 @@ import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <div className="p-5">
+        {/* TEXTO */}
         <h2 className="text-xl font-bold">Olá, Diego!</h2>
         <p>Domingo, 23 de Agosto.</p>
 
+        {/* BUSCA */}
         <div className="flex items-center gap-2 mt-6">
           <Input placeholder="Faça sua busca..." />
           <Button>
@@ -19,14 +24,38 @@ const Home = () => {
           </Button>
         </div>
 
+        {/* IMAGEM */}
         <div className="relative w-full h-[150px] mt-6">
           <Image
             alt="Agende nos melhores com FSW Barber"
             src="/banner-01.png"
             fill
-            className="rounded-lg object-cover"
+            className="rounded-xl object-cover"
           />
         </div>
+
+        {/* AGENDAMENTO */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-9">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">26</p>
+              <p className="text-sm">13:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
